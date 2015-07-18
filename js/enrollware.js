b873d1720b8753d22978202d1ba54814cabfb29a
Tim Arnold <jtim.arnold@gmail.com>
2015-07-18 14:32:11 -0400

    tidy html, add js for enroll ware

--- js/enrollware.js
+++ js/enrollware.js
@@ -88,3 +88,81 @@
     });
   }
 })(jQuery);
+
+var tcpr = (function () {
+  "use strict";
+  var courseIDs = {
+    'bls': [
+      { 'id': '7276',
+        'title': 'Healthcare Provider CPR (BLS) Initial'},
+      { 'id': '7275',
+        'title': 'Healthcare Provider CPR (BLS) Renewal'},
+      { 'id': '7286',
+        'title': 'Healthcare Provider CPR (BLS) Skills Check'}
+    ],
+    'acls': [
+      { 'id': '7278',
+        'title': 'ACLS- Initial'},
+      { 'id': '7277',
+        'title': 'ACLS Renewal'},
+      { 'id': '7475',
+        'title': 'ACLS Skills Check'},
+      { 'id': '7280',
+        'title': 'ACLS/BLS Initial'},
+      { 'id': '7279',
+        'title': 'ACLS/BLS Renewal'},
+      { 'id': '7284',
+        'title': 'PALS Initial'},
+      { 'id': '7285',
+        'title': 'PALS Renewal'},
+      { 'id': '7288',
+        'title': 'PALS Skills Check'}
+    ],
+    'heartsaver': [
+      { 'id': '7281',
+        'title': 'Heartsaver CPR'},
+      { 'id': '31031',
+        'title': 'Heartsaver CPR Skills Check'}
+    ],
+    'heartsaverfirstaid': [
+      { 'id': '7282',
+        'title': 'Adult First Aid & CPR'},
+      { 'id': '7293',
+        'title': 'Heartsaver First Aid'},
+      { 'id': '7283',
+        'title': 'Pediatric First Aid & CPR'},
+      { 'id': '31030',
+        'title': 'Pediatric First Aid & CPR Skills Check'}
+    ]
+  };
+  var getCourses = function(){
+    var fstem = window.location.pathname.split("/").pop().split('.')[0];
+    var courses = tcpr.getURL(fstem);
+    $.each(courses, function(idx, obj){
+      var elemString = '<div><h2 class="' + obj.id + '">' + obj.title + '</h2>';
+      elemString += '<div id="' + obj.id + '" style="display: none"></div></div>';
+      $("#coursemenu").append(elemString);
+      $("div#"+obj.id).enrollware({
+        feed:"https://trianglecpr.enrollware.com/registration/schedule-feed.ashx?courseid="+obj.id
+      });
+      $( "h2." + obj.id ).click(function() {
+        $( "div#"+obj.id ).toggle('fast');
+      });   
+    });
+    $(function(){
+      $( "#register" ).click(function() {
+        $( "#coursemenu" ).toggle('fast');
+      });   
+    });
+      
+  }
+  
+  return { 
+    getURL: function (name) {
+      return courseIDs[name];
+    },
+    getCourses: getCourses
+  };
+}());
+
+  
