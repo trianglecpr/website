593322b75e54b4ff22c4f1054970d79bdb446895
Tim Arnold <jtim.arnold@gmail.com>
2018-09-14 13:34:54 -0400

    locationid change

--- js/enrollware.js
+++ js/enrollware.js
@@ -1,7 +1,7 @@
 (function ($) {
   $.fn.enrollware = function (options) {
     var $empty_response = '';
-      if (options.location === "Triangle CPR-Durham") {
+      if (options.locationid !== 2733) {
         $empty_response = '<div>Coming soon to this location. Please see Raleigh/Cary location class list.</div>';
       } else {
         $empty_response = '<div>No matching classes were found. Please call for an appointment</div>'
